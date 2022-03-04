9dee7e66edcea627df0d023f554705cb24054a3c
Mohammad Shahab <shahabkovict@gmail.com>
2022-03-04 20:45:21 +0530

    authentication with navigation done

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ const path = require('path');
 
 const express = require('express');
 
+const session = require('express-session');
+const mongodbStore = require('connect-mongodb-session');
+
+const MongoDBStore = mongodbStore(session);
+
 const blogRoutes = require('./routes/blog');
 const db = require('./data/database');
 
 const app = express();
+
+const sessionStore = new MongoDBStore({
+  uri: "mongodb://localhost:27017",
+  databaseName: "blog",
+  collection: "session"
+});
 
 // Activate EJS view engine
 app.set('view engine', 'ejs');
@@ -13,6 +24,24 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true })); // Parse incoming request bodies
 app.use(express.static('public')); // Serve static files (e.g. CSS files)
+
+app.use(session({
+  secret: "This-is-my-secret-key",
+  resave: false,
+  saveUninitialized: false,
+  store: sessionStore
+}));
+
+app.use(async function(req, res, next){
+  const user = req.session.user;
+  const isAuth = req.session.isAuthenticated;
+  if(!user || !isAuth){
+    return next();
+  }
+  const userDoc = await db.getDb().collection('users').findOne({_id: user.id});
+  res.locals.isAuth = isAuth;
+  next();
+});
 
 app.use(blogRoutes);
 
--- routes/blog.js
+++ routes/blog.js
@@ -1,36 +1,41 @@
-const express = require('express');
-const mongodb = require('mongodb');
+const express = require("express");
+const mongodb = require("mongodb");
 
-const db = require('../data/database');
+const db = require("../data/database");
+
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
 
 const ObjectId = mongodb.ObjectId;
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
-  res.redirect('/posts');
+// R O U T E S
+
+router.get("/", function (req, res) {
+  res.redirect("/posts");
 });
 
-router.get('/posts', async function (req, res) {
+router.get("/posts", async function (req, res) {
   const posts = await db
     .getDb()
-    .collection('posts')
+    .collection("posts")
     .find({})
-    .project({ title: 1, summary: 1, 'author.name': 1 })
+    .project({ title: 1, summary: 1, "author.name": 1 })
     .toArray();
-  res.render('posts-list', { posts: posts });
+  res.render("posts-list", { posts: posts });
 });
 
-router.get('/new-post', async function (req, res) {
-  const authors = await db.getDb().collection('authors').find().toArray();
-  res.render('create-post', { authors: authors });
+router.get("/new-post", async function (req, res) {
+  const authors = await db.getDb().collection("authors").find().toArray();
+  res.render("create-post", { authors: authors });
 });
 
-router.post('/posts', async function (req, res) {
+router.post("/posts", async function (req, res) {
   const authorId = new ObjectId(req.body.author);
   const author = await db
     .getDb()
-    .collection('authors')
+    .collection("authors")
     .findOne({ _id: authorId });
 
   const newPost = {
@@ -45,60 +50,60 @@ router.post('/posts', async function (req, res) {
     },
   };
 
-  const result = await db.getDb().collection('posts').insertOne(newPost);
+  const result = await db.getDb().collection("posts").insertOne(newPost);
   console.log(result);
-  res.redirect('/posts');
+  res.redirect("/posts");
 });
 
-router.get('/posts/:id', async function (req, res, next) {
+router.get("/posts/:id", async function (req, res, next) {
   let postId = req.params.id;
 
   try {
     postId = new ObjectId(postId);
   } catch (error) {
-    return res.status(404).render('404');
+    return res.status(404).render("404");
     // return next(error);
   }
 
   const post = await db
     .getDb()
-    .collection('posts')
+    .collection("posts")
     .findOne({ _id: postId }, { summary: 0 });
 
   if (!post) {
-    return res.status(404).render('404');
+    return res.status(404).render("404");
   }
 
-  post.humanReadableDate = post.date.toLocaleDateString('en-US', {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
+  post.humanReadableDate = post.date.toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
   });
   post.date = post.date.toISOString();
 
-  res.render('post-detail', { post: post });
+  res.render("post-detail", { post: post });
 });
 
-router.get('/posts/:id/edit', async function (req, res) {
+router.get("/posts/:id/edit", async function (req, res) {
   const postId = req.params.id;
   const post = await db
     .getDb()
-    .collection('posts')
+    .collection("posts")
     .findOne({ _id: new ObjectId(postId) }, { title: 1, summary: 1, body: 1 });
 
   if (!post) {
-    return res.status(404).render('404');
+    return res.status(404).render("404");
   }
 
-  res.render('update-post', { post: post });
+  res.render("update-post", { post: post });
 });
 
-router.post('/posts/:id/edit', async function (req, res) {
+router.post("/posts/:id/edit", async function (req, res) {
   const postId = new ObjectId(req.params.id);
   const result = await db
     .getDb()
-    .collection('posts')
+    .collection("posts")
     .updateOne(
       { _id: postId },
       {
@@ -111,16 +116,175 @@ router.post('/posts/:id/edit', async function (req, res) {
       }
     );
 
-  res.redirect('/posts');
+  res.redirect("/posts");
 });
 
-router.post('/posts/:id/delete', async function (req, res) {
+router.post("/posts/:id/delete", async function (req, res) {
   const postId = new ObjectId(req.params.id);
   const result = await db
     .getDb()
-    .collection('posts')
+    .collection("posts")
     .deleteOne({ _id: postId });
-  res.redirect('/posts');
+  res.redirect("/posts");
 });
 
 module.exports = router;
+
+//u s e r   a u t h e n t i c a t i o n
+
+router.get("/sign-up", function (req, res) {
+  let sessioninputData = req.session.inputData;
+  if (!sessioninputData) {
+    sessioninputData = {
+      hasError: false,
+      email: "",
+      confirmEmail: "",
+      password: "",
+    };
+  }
+  req.session.inputData = null;
+  res.render("sign-up", { inputData: sessioninputData });
+});
+
+router.post("/signup", async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredConfirmEmail = userData.confirmEmail;
+  const enteredPassword = userData.password;
+
+  if (
+    !enteredEmail ||
+    !enteredConfirmEmail ||
+    !enteredPassword ||
+    enteredPassword.trim().length < 6 ||
+    enteredEmail !== enteredConfirmEmail ||
+    !enteredEmail.includes("@")
+  ) {
+    req.session.inputData = {
+      hasError: true,
+      message: "invalid input- please try again",
+      email: enteredEmail,
+      confirmEmail: enteredConfirmEmail,
+      password: enteredPassword,
+    };
+    req.session.save(function () {
+      console.log("Incorrect data");
+      res.redirect("/sign-up");
+    });
+    return;
+  }
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: enteredEmail });
+
+  if (existingUser) {
+    req.session.inputData = {
+      hasError: true,
+      message: "Email already in use",
+      email: enteredEmail,
+      confirmEmail: enteredConfirmEmail,
+      password: enteredPassword,
+    };
+    req.session.save(function () {
+      console.log("Email already in use");
+      return res.redirect("sign-up");
+    });
+
+    return;
+  }
+
+  const hashPassword = await bcrypt.hash(enteredPassword, 12);
+
+  const user = {
+    email: enteredEmail,
+    password: hashPassword,
+  };
+
+  await db.getDb().collection("users").insertOne(user);
+  console.log(user);
+  res.redirect("sign-in");
+});
+
+router.get("/sign-in", function (req, res) {
+  let sessioninputData = req.session.inputData;
+  if (!sessioninputData) {
+    sessioninputData = {
+      hasError: false,
+      email: "",
+      confirmEmail: "",
+      password: "",
+    };
+  }
+  req.session.inputData = null;
+  res.render("sign-in", {inputData: sessioninputData});
+});
+
+router.post("/signin", async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredPassword = userData.password;
+
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: enteredEmail });
+
+  if (!existingUser) {
+
+    req.session.inputData = {
+      hasError: true,
+      message: "Invalid email or password - Please try again",
+      email: enteredEmail,
+      password: enteredPassword,
+    }
+    req.session.save(function(){
+      console.log("invalid email or password");
+      res.redirect("/sign-in");
+    })
+    return;
+  }
+
+  const existingPassword = await bcrypt.compare(
+    enteredPassword,
+    existingUser.password
+  );
+
+  if (!existingPassword) {
+    req.session.inputData = {
+      hasError: true,
+      message: "Invalid email or password - Please try again",
+      email: enteredEmail,
+      password: enteredPassword,
+    }
+    req.session.save(function(){
+      console.log("invalid email or password");
+      res.redirect("/sign-in");
+    })
+    return;
+  }
+  req.session.user = { id: existingUser._id, email: existingUser.email };
+  req.session.isAuthenticated = true;
+  req.session.save(function () {
+    console.log("user authenticated");
+    res.redirect("/admin");
+  });
+});
+
+router.get("/admin", function (req, res) {
+  if (!req.session.isAuthenticated) {
+    return res.render("401");
+  }
+  console.log("you are on admin page");
+  res.render("admin");
+});
+
+router.get("/log-out", function (req, res) {
+  res.redirect("/logout");
+});
+
+router.post("/logout", function (req, res) {
+  req.session.user = null;
+  req.session.isAuthenticated = false;
+  res.redirect("/");
+});
